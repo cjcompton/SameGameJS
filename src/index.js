cbfd9d6b129a0b2386a47de919766901235f5e6b
Charlie Compton <compton@charlies-air.lan>
2023-11-09 15:35:06 -0800

    added / commands

--- src/index.js
+++ src/index.js
@@ -11,4 +11,35 @@ const client = new Client({
 	],
 });
 
+client.on('ready', (c) => {
+	console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+	const contents = message.content.toLowerCase();
+	
+	if(message.author.bot) {
+		return;
+	}
+
+	if(contents === 'hello') {
+		message.reply('Hi there!');
+	}
+});
+
+// these interactions are good because you can pull user information from it when they enter a / command
+client.on('interactionCreate', (interaction) => {
+	if(!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+	console.log(interaction.commandName);
+
+	if(interaction.commandName === 'hey') {
+		interaction.reply('Hey!')
+	}
+
+	if(interaction.commandName === 'ping') {
+		interaction.reply('Pong!')
+	}
+});
+
 client.login(process.env.API_KEY);
