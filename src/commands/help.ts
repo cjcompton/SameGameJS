3becf6cefd7bacc56fff5511c1028e7237de5eda
Noah Myers <nmyers00@gmail.com>
2023-11-25 19:19:50 -0800

    update guide

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,6 +5,6 @@ export const data = new SlashCommandBuilder()
   .setDescription("How to use this bot.");
 
 export async function execute(interaction: CommandInteraction) {
-  const reply = "# How To Use:\n1. Link Steam to Discord ([Guide](https://support.discord.com/hc/en-us/articles/8063233404823-Connections-Linked-Roles-Community-Members#h_01GK285ENTCX37J9PYCM1ADXCH)).\n2. Use the `/auth` command.\n3. Use the `/samegame` command."
+  const reply = "# [Guide](https://sharedsteamgames.com?guide=true)"
   return interaction.reply({ content: reply, ephemeral: true });
 }
